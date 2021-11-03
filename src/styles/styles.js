69f9e2ad4c6fde52928ac79fd5b9ec964dd0a727
AntoineLemlin <ant.lemlin@gmail.com>
2021-11-03 19:36:45 +0100

    resize images on heroku

--- src/styles/styles.js
+++ src/styles/styles.js
@@ -87,8 +87,7 @@ export const useStyles = makeStyles((theme) => ({
   },
   media: {
     height: "50px",
-    width: "auto",
-    width: "auto",
+    width: "50px",
     marginLeft: "10px",
   },
   content: {
